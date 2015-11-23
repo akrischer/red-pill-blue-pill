5b372507ac38aa4374031691f2d9572042b2d0b6
akrischer <krischer.a@husky.neu.edu>
2015-11-23 13:32:52 -0500

    initial commit

---
+++ Assets/Assets/Scripts/Debug/TestUserOptionsSpriteText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestUserOptionsSpriteText : MonoBehaviour {
+
+    public bool runTest = false;
+
+	// Use this for initialization
+	void Start () {
+        UserOptionsSpriteText.create("hello");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
